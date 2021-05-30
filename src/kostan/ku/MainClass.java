31fa39a79c3b05cc8152bc95395f1241532658a6
auliawidaniq <auddyauraflestari@gmail.com>
2021-05-30 10:23:47 +0700

    first commit

---
+++ src/kostan/ku/MainClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kostan.ku;
+
+/**
+ *
+ * @author Aulia
+ */
+public class MainClass {
+    public static void main (String[]args){
+       Awal awal = new Awal();
+       awal.setVisible(true);
+    }
+    
+}
